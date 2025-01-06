7df80d598ab0658eb7f68185588629699102fbb3
Mohendra12 <mohendrathapa418@gmail.com>
2025-01-06 11:57:35 +0530

    "Initial commit"

---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', 
+function() {
+    const coll = document.querySelectorAll('.collapsible');
+    coll.forEach(function(item) {
+        item.addEventListener('click', function()
+        {
+            this.classList.toggle('acitve');
+            const content = this.nextElementSibling;
+            if (content.style.display === 'block')
+            {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const coll = doucment.querySelectorAll('.collap');
+    coll.forEach(function(item){
+        item.addEventListener('click', function()
+    {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        if (content.style.display === 'block')
+        {
+            content.style.display = 'none';
+
+        } else {
+            content.style.display = 'block';
+        }
+
+    });
+    });
+});
